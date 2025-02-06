eb741355ce30c3f70f4cd5abac6d1821e85faa2d
Ansh2 <ansh.nynj@gmail.com>
2025-02-05 21:23:56 -0800

    making new changes to theme context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,1 +1,22 @@
- 
+import React, { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle("light-mode");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
