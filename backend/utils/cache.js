5705c20e5f3acf8cc9a6c0aa7e14d0cccde691c0
Ansh2 <ansh.nynj@gmail.com>
2025-02-06 20:29:57 -0800

    cache for robust video processing in utils

--- backend/utils/cache.js
+++ backend/utils/cache.js
@@ -1,1 +1,6 @@
- 
+import NodeCache from "node-cache";
+
+export const cache = new NodeCache({
+  stdTTL: 3600, // Default TTL: 1 hour
+  checkperiod: 600, // Check for expired keys every 10 minutes
+});
