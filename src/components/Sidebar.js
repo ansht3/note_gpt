bcdb0d30945bca660547a57924a29d505dbbeec9
Ansh2 <ansh.nynj@gmail.com>
2025-06-15 23:53:52 -0500

    minor change to the frontend

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -448,7 +448,7 @@ function Sidebar() {
         {/* Shortcuts Menu */}
         {showShortcutsMenu && (
           <div ref={shortcutsMenuRef} className="shortcuts-menu animated-menu">
-            <h3>Keyboard Shortcuts</h3>
+            <h3>Keyboard Shortcut</h3>
             {Object.entries(shortcuts).map(([key, description]) => (
               <div key={key} className="shortcut-item">
                 <kbd>{key}</kbd>
